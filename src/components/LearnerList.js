257a478801818f17c0f6c4f1db55efd9b37a8896
Redwan Ahmed <redwanahmed1@hotmail.co.uk>
2021-05-06 18:23:26 +0100

    Set up react and firebase

---
+++ src/components/LearnerList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import firebase from '../firebase';
+
+const LearnerList = () => {
+
+    return (
+        <div>Learner List</div>
+    )
+}
+
+export default LearnerList;
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDuCBenU-J78uWJm9vANTUeR93tDFkg98M",
+    authDomain: "learner-app-79c7d.firebaseapp.com",
+    projectId: "learner-app-79c7d",
+    storageBucket: "learner-app-79c7d.appspot.com",
+    messagingSenderId: "738823945870",
+    appId: "1:738823945870:web:e815b2037bf730622013df",
+    measurementId: "G-HTFSJ6ZE46"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
